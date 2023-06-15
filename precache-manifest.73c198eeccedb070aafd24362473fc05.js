49854f5e1888081e0de2ca403ae2d9337707f4ad
j3553pat <jessecodedev@gmail.com>
2023-06-14 19:57:21 -0500

    Updates

---
+++ precache-manifest.73c198eeccedb070aafd24362473fc05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d8a7aa743ea49d7b484058b847c2f0d",
+    "url": "/411_wk5_day2_mapdispatchtoprops/index.html"
+  },
+  {
+    "revision": "146d28e16e298e4ce67e",
+    "url": "/411_wk5_day2_mapdispatchtoprops/static/css/main.be9515fe.chunk.css"
+  },
+  {
+    "revision": "55998cca79cd376335d1",
+    "url": "/411_wk5_day2_mapdispatchtoprops/static/js/2.a9e00edc.chunk.js"
+  },
+  {
+    "revision": "146d28e16e298e4ce67e",
+    "url": "/411_wk5_day2_mapdispatchtoprops/static/js/main.ec7fb2a5.chunk.js"
+  },
+  {
+    "revision": "e47b0be5e9874947a62d",
+    "url": "/411_wk5_day2_mapdispatchtoprops/static/js/runtime~main.df0822c9.js"
+  }
+]);
